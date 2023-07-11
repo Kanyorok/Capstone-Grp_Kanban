58a31bb7c75f67ee49f79aa36e3d699ddf9b40f2
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-07-11 16:58:37 +0300

    fix Eslint error

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,7 +2,7 @@ import createTask from './display.js';
 
 const apiData = 'https://api.tvmaze.com/shows';
 
-const showMovies= async () => {
+const showMovies = async () => {
   try {
     const retrieveData = await fetch(apiData);
     const retrievedData = await retrieveData.json();
