7c569cafc2d5c39487ff95dd9ab421ee57adab58
Peter-Kibet <peter.kibet@hotmail.com>
2023-07-14 13:23:01 +0300

    Update reservation popup.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import "./styles/style.css";
 import showMovies from "./modules/api.js";
-import { showPopup, comments } from "./modules/commentPopup.js";
-import { reservationPopup, reservations } from "./modules/reservationPopup.js";
+import { comments } from "./modules/commentPopup.js";
+import {
+  createReservationPopup,
+  reservations,
+} from "./modules/reservationPopup.js";
 
 window.addEventListener("load", () => {
   showMovies();
   document.addEventListener("click", comments);
-  document.getElementById("popWindow").innerHTML = showPopup();
+
+  const reservationWindow = document.getElementById("reservationWindow");
+  reservationWindow.appendChild(createReservationPopup());
 
   document.addEventListener("click", reservations);
-  document.getElementById("reservationWindow").innerHTML = reservationPopup();
 });
--- src/modules/reservationPopup.js
+++ src/modules/reservationPopup.js
@@ -2,62 +2,133 @@ import closeSvg from "../assets/images/close.svg";
 
 const baseAPI = "https://api.tvmaze.com/shows";
 
-export const reservationPopup = () => {
-  const pop = `<div class="popup">
-    <div class="popup_content">
-      <div class="titleContent">
-        <div class="heading">
-          <h2 id="headingSection"></h2>
-        </div>
-        <img id="closeButton" class="closeFunction" src="" alt="">
-      </div>
-      <div class="desktopView">
-        <div id="popup_info">
-          <img id="popup-img" src="" alt="Portfolio">
-        </div>
-        <div class ="all_info">
-          <h3>Summary</h3>
-          <p id="projectDesc"></p>
-          <div class="bottom_section">
-            <h3>Reserve Now!</h3>
-            <form action="" method="post" id="form" class="contact_form">
-              <input name="FullName" type="text" placeholder="Full name" required maxlength="30" />
-              <textarea name="Message" rows="4" cols="50" placeholder="Your Insights...." maxlength="500" required></textarea>
-              <div class="centered_button" id="form-btn-container">
-                <button id="form_btn" type="submit">Comment</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>`;
-  return pop;
+export const createReservationPopup = () => {
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  const popupContent = document.createElement("div");
+  popupContent.classList.add("popup_content");
+
+  const titleContent = document.createElement("div");
+  titleContent.classList.add("titleContent");
+
+  const heading = document.createElement("div");
+  heading.classList.add("heading");
+
+  const headingSection = document.createElement("h2");
+  headingSection.id = "headingSection";
+  heading.appendChild(headingSection);
+
+  const closeButton = document.createElement("img");
+  closeButton.id = "closeButton";
+  closeButton.classList.add("closeFunction");
+  closeButton.src = closeSvg;
+  heading.appendChild(closeButton);
+
+  titleContent.appendChild(heading);
+
+  const desktopView = document.createElement("div");
+  desktopView.classList.add("desktopView");
+
+  const popupInfo = document.createElement("div");
+  popupInfo.id = "popup_info";
+
+  const popupImg = document.createElement("img");
+  popupImg.id = "popup-img";
+  popupImg.alt = "Portfolio";
+  popupInfo.appendChild(popupImg);
+
+  const allInfo = document.createElement("div");
+  allInfo.classList.add("all_info");
+
+  const summaryHeading = document.createElement("h3");
+  summaryHeading.textContent = "Summary";
+  allInfo.appendChild(summaryHeading);
+
+  const projectDesc = document.createElement("p");
+  projectDesc.id = "projectDesc";
+  allInfo.appendChild(projectDesc);
+
+  const bottomSection = document.createElement("div");
+  bottomSection.classList.add("bottom_section");
+
+  const reserveHeading = document.createElement("h3");
+  reserveHeading.textContent = "Reserve Now!";
+  bottomSection.appendChild(reserveHeading);
+
+  const form = document.createElement("form");
+  form.action = "";
+  form.method = "post";
+  form.id = "form";
+  form.classList.add("contact_form");
+
+  const fullNameInput = document.createElement("input");
+  fullNameInput.name = "FullName";
+  fullNameInput.type = "text";
+  fullNameInput.placeholder = "Full name";
+  fullNameInput.required = true;
+  fullNameInput.maxLength = 30;
+  form.appendChild(fullNameInput);
+
+  const messageTextarea = document.createElement("textarea");
+  messageTextarea.name = "Message";
+  messageTextarea.rows = 4;
+  messageTextarea.cols = 50;
+  messageTextarea.placeholder = "Your Insights....";
+  messageTextarea.required = true;
+  messageTextarea.maxLength = 500;
+  form.appendChild(messageTextarea);
+
+  const formBtnContainer = document.createElement("div");
+  formBtnContainer.id = "form-btn-container";
+  form.appendChild(formBtnContainer);
+
+  const formBtn = document.createElement("button");
+  formBtn.id = "form_btn";
+  formBtn.type = "submit";
+  formBtn.textContent = "Comment";
+  formBtnContainer.appendChild(formBtn);
+
+  bottomSection.appendChild(form);
+
+  allInfo.appendChild(bottomSection);
+  desktopView.appendChild(popupInfo);
+  desktopView.appendChild(allInfo);
+  popupContent.appendChild(titleContent);
+  popupContent.appendChild(desktopView);
+  popup.appendChild(popupContent);
+
+  return popup;
 };
-const closed = () => {
-  const popWin = document.querySelector(".popup");
-  popWin.style.visibility = "hidden";
-  return null;
+
+const closePopup = () => {
+  const popup = document.querySelector(".popup");
+  popup.style.visibility = "hidden";
 };
+
 export const reservations = async (e) => {
-  const worksImg = document.getElementById("closeButton");
-  worksImg.src = closeSvg;
+  const closeButton = document.getElementById("closeButton");
+  closeButton.src = closeSvg;
+
   if (e && e.target && e.target.classList.contains("reservations")) {
     const buttonId = parseInt(e.target.id, 10);
-    const titleSelect = document.getElementById("headingSection");
-    const imageSelect = document.getElementById("popup-img");
-    const paragraphSelect = document.getElementById("projectDesc");
-    const popWin = document.querySelector(".popup");
+    const headingSection = document.getElementById("headingSection");
+    const popupImg = document.getElementById("popup-img");
+    const projectDesc = document.getElementById("projectDesc");
+    const popup = document.querySelector(".popup");
+
     const response = await fetch(baseAPI);
     const retrievedData = await response.json();
+
     retrievedData.forEach((show) => {
       if (show.id === buttonId) {
-        titleSelect.innerHTML = show.name;
-        paragraphSelect.innerHTML = show.summary;
-        imageSelect.src = show.image.medium;
-        popWin.style.visibility = "visible";
+        headingSection.innerHTML = show.name;
+        projectDesc.innerHTML = show.summary;
+        popupImg.src = show.image.medium;
+        popup.style.visibility = "visible";
       }
     });
-    worksImg.addEventListener("click", closed);
+
+    closeButton.addEventListener("click", closePopup);
   }
 };
